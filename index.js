bd60717b8eccc1fc05dd6e160698051c1605d087
HariomRajpoot1 <147224797+HariomRajpoot1@users.noreply.github.com>
2023-10-07 08:22:27 +0000

    type data conversion

---
+++ index.js
@@ -0,0 +1,13 @@
+"use Strict";//
+let a = "20"
+// Type conversion
+/*
+let b = 
+Number(a);  // for convert number
+String(a)   // for string
+Boolean(a)   // for true/false
+*/
+
+let c =  3;
+let d = Boolean(c);
+console.log(d);
